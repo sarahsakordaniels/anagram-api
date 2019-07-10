ca507d1571615db757ca12753090954ca68c2578
sarahsakordaniels <ssdaniels13@gmail.com>
2019-07-09 18:06:57 -0600

    adjust seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,10 @@
 File.foreach('./lib/dictionary.txt') do |line|
   word = line.strip
   anagram = Anagram.find_or_create_by(key: word.downcase.chars.sort.join)
-  anagram.words.create(text: word)
+end
+
+
+File.foreach('./lib/dictionary.txt') do |line|
+  word = line.strip
+  Word.find_or_create_by(spelling: word)
 end
