a00caf85784fdaeeddf0b82d0609970d24c986fe
sarahsakordaniels <ssdaniels13@gmail.com>
2019-07-10 14:39:49 -0600

    working get and optional limit param

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -1,7 +1,7 @@
 class AnagramsController < ApplicationController
   #GET anagrams for given word
-  def index
-    render json: {"Anagrams": anagram_list}
+  def show
+    render json: {"anagrams": anagram_list}
   end
 
   private
@@ -9,31 +9,32 @@ class AnagramsController < ApplicationController
   def anagram_params
     params.permit(:spelling, :limit)
   end
+
   # finds the word passed in the params
   def find_word
-    Word.find_by(spelling: params[:spelling])
+    word = Word.find_by(spelling: params[:spelling])
   end
-  # If word exists, it checks for a limit. If word does not exist, error is returned
+
+  #checks for a limit param. If none, run entire list of available anagrams
+  def limit_anagrams?
+    if params[:limit]
+      all_anagrams[0,params[:limit].to_i]
+    else
+      all_anagrams
+    end
+  end
+
+#if the word exists, checks for limit param. From there, the limit_anagrams? method will render json. if the word does not exist, error message will appear.
   def anagram_list
     if word
-      limit?
+      limit_anagrams?
     else
       ["That word does not exist in our database!"]
     end
   end
 
-  # limits the number of results if the limit param is passed
-  def limit?
-    if params[:limit]
-      anagrams[0,params[:limit].to_i]
-    else
-      anagrams
-    end
-  end
-  # Finds all words associated with the sorted key, and then removes the
-  # word itself because a word is not its own anagram by definition.
-  def anagrams
-    searched_word = word.anagram
-    searched_word.words.pluck(:spelling) - [word.spelling]
+  #finds all words associated with the search param via anagram key, then removes the searched word because it is not an anagram of itself
+  def all_anagrams
+    word.anagram.words.pluck(:spelling) - [word.spelling]
   end
 end
