b55fa6e7cabaf3f2150f5c4fc641c3401072d361
sarahsakordaniels <ssdaniels13@gmail.com>
2019-07-09 18:15:18 -0600

    after first migration

--- db/migrate/20190709234949_create_anagrams.rb
+++ db/migrate/20190709234949_create_anagrams.rb
@@ -1,7 +1,8 @@
 class CreateAnagrams < ActiveRecord::Migration[5.2]
   def change
     create_table :anagrams do |t|
-      t.string :key
+        t.string :key
+        t.text :words, array: true, default: []
     end
   end
 end
