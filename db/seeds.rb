b3aa33f124ce3dc98f017c77b34b41949de1dbce
sarahsakordaniels <ssdaniels13@gmail.com>
2019-07-12 16:49:55 -0600

    edit readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Anagram.destroy_all
 #If a key (a split and alphabetized version of the word) is not found for the word, it is created and the word is assigned to that key
 #If a key exists, the word is assigned to that existing key
 
-File.foreach('./lib/dictionary.txt') do |line|
+File.foreach('./lib/short_dictionary.txt') do |line|
   word = line.strip
   anagram = Anagram.find_or_create_by(key: word.downcase.split(//).sort.join)
   anagram.words.create(spelling: word)
